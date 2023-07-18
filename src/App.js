9005ffa59082eeda273ee86ea1db3d1e7ca44dcd
venugopal <venugopal@technqtechnologies.com>
2023-07-18 09:52:46 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
 
 function App() {
+  const [count ,setCount] = useState(100);
+  function increase(){
+    setCount(count +1)
+  }
+
+  function decrease(){
+    setCount(count-1)
+  }
+  function reset(){
+    setCount(0)
+  }
+  function SIX(){
+    setCount(count +6)
+  }
+  function FOUR(){
+    setCount(count +4)
+  }
+  
+  function EXTRA(){
+    setCount(count +1)
+  }
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='container'>
+    <h1 className='heading'>SCORE : {count} </h1>
+    <button className='btn'onClick={increase}>RUNS</button>  &nbsp; {count} &nbsp;
+    <button  className='btn' onClick={decrease}>-</button> &nbsp;
+    <button className='btn' onClick={reset}>Reset</button><br></br>
+    <button  className='btn' onClick={SIX}>SIX</button><br></br>
+    <button className='btn' onClick={FOUR}>FOUR</button><br></br>
+    <button className='btn' onClick={EXTRA}>EXTRA</button><br></br>
+  </div>
   );
 }
 
---
+++ src/Score/Cricket.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Cricket.css'
+
+function Cricket() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Cricket
